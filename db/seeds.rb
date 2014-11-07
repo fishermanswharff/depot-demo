c561fde6e9e660f3006fcdffb94d2add0681c352
fishermanswharff <fishermanswharff@github.com>
2014-11-06 22:30:55 -0500

    initial app setup, product scaffolding

---
+++ db/seeds.rb
@@ -0,0 +1,50 @@
+Product.delete_all
+
+Product.create!(
+  title: 'Javascript Patterns',
+  description: %{What's the best approach for developing an application with JavaScript? This book helps you answer that question with numerous JavaScript coding patterns and best practices. If you're an experienced developer looking to solve problems related to objects, functions, inheritance, and other language-specific categories, the abstractions and code templates in this guide are ideal—whether you're using JavaScript to write a client-side, server-side, or desktop application.},
+  image_url: 'http://ecx.images-amazon.com/images/I/51ACzMjH6rL.jpg',
+  price: 19.01
+)
+
+Product.create!(
+  title: 'Functional JavaScript: Introducing Functional Programming with Underscore.js',
+  description: %{How can you overcome JavaScript language oddities and unsafe features? With this book, you’ll learn how to create code that’s beautiful, safe, and simple to understand and test by using JavaScript’s functional programming support. Author Michael Fogus shows you how to apply functional-style concepts with Underscore.js, a JavaScript library that facilitates functional programming techniques. Sample code is available on GitHub at https://github.com/funjs/book-source.},
+  image_url: 'http://ecx.images-amazon.com/images/I/51ST81NQRiL.jpg',
+  price: 21.61
+)
+
+Product.create!(
+  title: 'JavaScript: The Good Parts',
+  description: %{Most programming languages contain good and bad parts, but JavaScript has more than its share of the bad, having been developed and released in a hurry before it could be refined. This authoritative book scrapes away these bad features to reveal a subset of JavaScript that's more reliable, readable, and maintainable than the language as a whole—a subset you can use to create truly extensible and efficient code.},
+  image_url: 'http://ecx.images-amazon.com/images/I/71M32wxOOmL.jpg',
+  price: 17.39
+)
+
+Product.create!(
+  title: 'JavaScript: The Definitive Guide: Activate Your Web Pages (Definitive Guides)',
+  description: %{The 6th edition covers HTML5 and ECMAScript 5. Many chapters have been completely rewritten to bring them in line with today's best web development practices. New chapters in this edition document jQuery and server side JavaScript. It's recommended for experienced programmers who want to learn the programming language of the Web, and for current JavaScript programmers who want to master it.},
+  image_url: 'http://ecx.images-amazon.com/images/I/51WD-F3GobL.jpg',
+  price: 30.83
+)
+
+Product.create!(
+  title: 'Professional JavaScript for Web Developers',
+  description: %{This book provides a developer-level introduction along with more advanced and useful features of JavaScript. Coverage includes:},
+  image_url: 'http://ecx.images-amazon.com/images/I/51bRhyVTVGL.jpg',
+  price: 26.76
+)
+
+Product.create!(
+  title: 'Secrets of the JavaScript Ninja',
+  description: %{Secrets of the Javascript Ninja takes you on a journey towards mastering modern JavaScript development in three phases: design, construction, and maintenance. Written for JavaScript developers with intermediate-level skills, this book will give you the knowledge you need to create a cross-browser JavaScript library from the ground up},
+  image_url: 'http://ecx.images-amazon.com/images/I/51UYwOhvQPL.jpg',
+  price: 25.00
+)
+
+Product.create!(
+  title: 'Learning JavaScript Design Patterns',
+  description: %{With Learning JavaScript Design Patterns, you’ll learn how to write beautiful, structured, and maintainable JavaScript by applying classical and modern design patterns to the language. If you want to keep your code efficient, more manageable, and up-to-date with the latest best practices, this book is for you.},
+  image_url: 'http://ecx.images-amazon.com/images/I/51jEMnP6UGL.jpg',
+  price: 25.36
+)
